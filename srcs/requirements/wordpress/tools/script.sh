1ff008c6bf02ede930aa166de097a5d099043049
yel-mrab <yahya181elmrabti@gmail.com>
2022-11-09 20:35:52 +0100

    refactor: wordpress docker file

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,5 +1,13 @@
 #! /bin/sh
 
+wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+
+chmod +x wp-cli.phar
+
+mv wp-cli.phar /usr/local/bin/wp
+
+wp core download --allow-root
+
 wp --allow-root core config --dbhost=mariadb --dbname=wordpress --dbuser=root --dbpass=$SQLROOTPASS
 
 wp --allow-root core install --url=localhost --title="inceptin" --admin_user=$WPUSR --admin_email=$WPEMAIL
