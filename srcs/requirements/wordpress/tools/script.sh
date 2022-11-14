adfb8307e7288287d0d59d60571a8230f32049c7
yahya el mrabti <yahya@yel-mrab.42.fr>
2022-11-14 17:02:55 -0600

    fix: wordpress sctipt

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,7 +1,5 @@
 #! /bin/sh
 
-apt install -y php7.3 php-mysqli mariadb-client php-fpm wget
-
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 
 chmod +x wp-cli.phar
@@ -14,11 +12,9 @@ wp --allow-root core config --dbhost=mariadb --dbname=wordpress --dbuser=root --
 
 chmod 777 /var/www/html/wp-config.php
 
-wp --allow-root core install --url=localhost --title="$WPTITLE" --admin_user=$WPUSR --admin_password=$WPPASS --admin_email=$WPEMAIL
+wp --allow-root core install --url=$DOMAIN_NAME --title=$WPTITLE --admin_user=$WPUSR --admin_password=$WPPASS --admin_email=$WPEMAIL
 
 wp --allow-root plugin install redis-cache --activate
-
-wp --allow-root theme install twentysixteen --activate
 
 sed -i "40i define( 'WP_REDIS_HOST', 'redis' );"			/var/www/html/wp-config.php
 sed -i "42i define( 'WP_REDIS_PASSWORD', '$REDISPASS' );"	/var/www/html/wp-config.php
